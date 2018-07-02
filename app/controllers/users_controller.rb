231f28eebafaa8dc9e14b202e7ca877cddd65784
github username <github email address>
2018-07-02 12:25:13 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.find_or_create_by(user_params)
+        @user.save
+        redirect_to user_path(@user)
+    end
+
+    def show
+        @user = User.find(user_params)
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :password, :height, :tickets, :happiness, :nausea)
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+    root 'static#index'
+    get '/users', to: 'users#new'
+    post '/users', to: 'users#create'
+    get '/users/:id', to: 'users#show'
+
+    
     # get 'attractions', 'attractions#index'
     # get 'attractions', 'attractions#new'
     # post 'attractions', 'attractions#create'
     # get 'attractions/:id', 'attractions#show'
+
 end
